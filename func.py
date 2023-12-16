cd94bad6b3f69e6936cf5166edae15fd82cd2748
yanjianyu <545461426@qq.com>
2023-12-16 22:20:05 +0800

    fix: fix

--- func.py
+++ func.py
@@ -4,8 +4,11 @@ import numpy as np
 class Function:
     def __call__(self,input):
         x = input.data
+        self.input = input
         y = self.forward(x)
         output = variable.Variable(y)
+        self.output = output
+        self.output.set_creator(self)
         return output
     def forward(self,x):
         raise NotImplementedError()
@@ -13,10 +16,15 @@ class Function:
 class Square(Function):
     def forward(self,x):
         return x ** 2
+    def backword(self,dy):
+        return 2*self.input.data*dy
 
 class Exp(Function):
     def forward(self,x):
         return np.exp(x.data)
+    def backword(self,dy):
+        x = self.input.data
+        return np.exp(x.data)*dy
 
 if __name__ == '__main__':
     x = variable.Variable(np.array(10))
--- variable.py
+++ variable.py
@@ -1,5 +1,14 @@
 class Variable:
     def __init__(self,data):
         self.data = data
+        # 这个值带的梯度
+        self.grad = None
+        # 就是这个值的计算者，反向传播回去的时候，往回传播s
+        self.creator = None
+    def set_creator(self,f):
+        '''
+        这里的f是算子也就是func的functions
+        '''
+        self.creator = f
 
     
