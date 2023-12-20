350848b9e5878341ae427486b3f0cb73ccb4dfd8
yanjianyu <545461426@qq.com>
2023-12-20 09:26:35 +0800

    feat: update

--- func.py
+++ func.py
@@ -1,32 +1,60 @@
 import variable
 import numpy as np
 
+
 class Function:
-    def __call__(self,input):
-        x = input.data
-        self.input = input
-        y = self.forward(x)
-        output = variable.Variable(y)
-        self.output = output
-        self.output.set_creator(self)
-        return output
-    def forward(self,x):
+    def __call__(self,*inputs):
+        self.inputs = [input.data for input in inputs]
+        assert len(self.inputs) > 0
+
+        self.generation = 0
+        for input in inputs:
+            self.generation = max(self.generation,input.generation)
+
+        ys = self.forward(self.inputs)
+        outputs = [variable(ys,self.generation+1) for y in ys] 
+
+        return outputs
+    def forward(self,*xs):
         raise NotImplementedError()
 
 class Square(Function):
     def forward(self,x):
+        assert len(self.inputs) == 1
         return x ** 2
     def backword(self,dy):
         return 2*self.input.data*dy
 
+
+def square(x):
+    return Square()(x)
+
 class Exp(Function):
     def forward(self,x):
+        assert len(self.inputs) == 1
         return np.exp(x.data)
     def backword(self,dy):
         x = self.input.data
         return np.exp(x.data)*dy
+def exp(x):
+    return exp()(x)
+
+def SquareSun(Function):
+    def forward(self,x0,x1):
+        assert len(self.inputs) == 2
+        return 2*x0*x0+x1*x1
+    def backforward(self,dy):
+        x0 = self.inputs[0]
+        x1 = self.inputs[1]
+        return 4*x0+2*x1
+
+def squaresum(x,b):
+    return SquareSun()(x,b)
+
 
 if __name__ == '__main__':
-    x = variable.Variable(np.array(10))
-    f = Exp()
-    print(f(x).data)
+    x = variable.Variable(np.array(10),0)
+    a = square(x) 
+    y = squaresum(x,a)
+    print(y.data)
+    
--- variable.py
+++ variable.py
@@ -1,14 +1,24 @@
 class Variable:
-    def __init__(self,data):
+    def __init__(self,data,generation):
         self.data = data
         # 这个值带的梯度
         self.grad = None
         # 就是这个值的计算者，反向传播回去的时候，往回传播s
         self.creator = None
-    def set_creator(self,f):
+        # 代数
+        self.generation = generation
+    def set_creator(self,f,generation):
         '''
         这里的f是算子也就是func的functions
         '''
         self.creator = f
+        self.generation = generation
+    
+    def backforward(self,gy):
+        return 
+        #funcs = [self.creator]
+        #while funcs:
+            
+        
 
     
