9a75a0a2c2dae5f1ca84986c5c13be942c28d5bd
yanjianyu <545461426@qq.com>
2023-12-20 09:34:54 +0800

    fix: fix

--- func.py
+++ func.py
@@ -5,6 +5,7 @@ import numpy as np
 class Function:
     def __call__(self,*inputs):
         self.inputs = [input.data for input in inputs]
+        print(inputs)
         assert len(self.inputs) > 0
 
         self.generation = 0
@@ -12,7 +13,7 @@ class Function:
             self.generation = max(self.generation,input.generation)
 
         ys = self.forward(self.inputs)
-        outputs = [variable(ys,self.generation+1) for y in ys] 
+        outputs = [variable.Variable(ys,self.generation+1) for y in ys] 
 
         return outputs
     def forward(self,*xs):
@@ -21,9 +22,9 @@ class Function:
 class Square(Function):
     def forward(self,x):
         assert len(self.inputs) == 1
-        return x ** 2
+        return [x[0] ** 2]
     def backword(self,dy):
-        return 2*self.input.data*dy
+        return [2*self.input.data*dy]
 
 
 def square(x):
@@ -32,21 +33,21 @@ def square(x):
 class Exp(Function):
     def forward(self,x):
         assert len(self.inputs) == 1
-        return np.exp(x.data)
+        return [np.exp(x[0].data)]
     def backword(self,dy):
         x = self.input.data
-        return np.exp(x.data)*dy
+        return [np.exp(x[0].data)*dy]
 def exp(x):
     return exp()(x)
 
-def SquareSun(Function):
-    def forward(self,x0,x1):
+class SquareSun(Function):
+    def forward(self,x):
         assert len(self.inputs) == 2
-        return 2*x0*x0+x1*x1
+        return [2*x[0]*x[0]+x[1]*x[1]]
     def backforward(self,dy):
         x0 = self.inputs[0]
         x1 = self.inputs[1]
-        return 4*x0+2*x1
+        return [4*x[0]+2*x[1]]
 
 def squaresum(x,b):
     return SquareSun()(x,b)
@@ -55,6 +56,6 @@ def squaresum(x,b):
 if __name__ == '__main__':
     x = variable.Variable(np.array(10),0)
     a = square(x) 
-    y = squaresum(x,a)
-    print(y.data)
+    #y = squaresum(x,a)
+    print(a[0].data)
     
